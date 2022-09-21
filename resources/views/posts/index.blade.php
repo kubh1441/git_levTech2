a5447507ceac36675f20c5c8ace01b093fee2a1e
kubh1441 <kubh1441@gmail.com>
2022-09-21 23:48:25 +0900

    カリキュラム08-2課題2

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Blog</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    </head>
+    <body>
+        <h1>Blog Name</h1>
+        @foreach($posts as $post)　<!-- コントローラで指定した変数$postsなので、$posts as $postとする -->
+        <div class='posts'>        <!-- これで$posts一つ一つのデータをforeachブロック内で$postとして扱える -->
+            <div class='post'>
+                <h2 class='title'>{{$post->title}}</h2><!-- 取り出された変数$postのデータのtitleカラム。titleは$postインスタンスのプロパティになっている。 -->
+                <p class='body'>{{$post->body}}</p><!-- 取り出された変数$postのデータのbodyカラム。bodyは$postインスタンスのプロパティになっている。 -->
+            </div>
+        </div>
+        @endforeach
+    </body>
+</html>
