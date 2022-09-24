1a95d81a3220282de49466e7a9f67fdafa2d3b1d
kubh1441 <kubh1441@gmail.com>
2022-09-24 23:28:58 +0900

    ADD 記事投稿作成画面

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,13 +10,18 @@ class PostController extends Controller
 
     public function index(Post $post)
     {
-        return view('posts.index')->with(['posts' => $post->getPaginateByLimit(1)]);
+        return view('posts.index')->with(['posts' => $post->getPaginateByLimit(5)]);
     }
 
     public function show(Post $post)
     {
         return view('posts.show')->with(['post' => $post]);//show関数で対象のPostsテーブルデータを取得する
         //'post'はshow.blade.phpで使う変数。中身の$postはid=1のPostインスタンス
+    }
+    
+    public function create()
+    {
+        return view('posts.create');
     }
     
 }
---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Blog</title>
+    </head>
+    <body>
+        <h1>Blog Name</h1>
+        <form action="/posts" method="POST">
+            @csrf
+            <div class="title">
+                <h2>Title</h2>
+                <input type="text" name="post[title]" placeholder="タイトル"/><!-- name属性はサーバ側で扱うキーとなることを意識する -->
+            </div><!-- 今回はpost情報であることを明確にするため、post[title]とする。 -->
+            <div class="body">
+                <h2>Body</h2>
+                <textarea name="post[body]" placeholder="今日も1日お疲れさまでした。"></textarea>
+            </div>
+            <input type="submit" value="store"/>
+        </form>
+        <div class="footer">
+            <a href="/">戻る</a>
+        </div>
+    </body>
+</html>
